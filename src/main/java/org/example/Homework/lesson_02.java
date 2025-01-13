9961209c1f6814468352daf0edba47a1afe303f8
mavluda <m.raximquliyeva@uzinfocom.uz>
2025-01-13 17:58:48 +0500

    lesson 2 Homework

--- src/main/java/org/example/Homework/lesson_02.java
+++ src/main/java/org/example/Homework/lesson_02.java
@@ -2,4 +2,26 @@ package org.example.Homework;
 
 public class lesson_02 {
 
+  public static void main(String[] args) {
+
+    byte b1 = 12;
+    byte b2 = 0b1100;
+    byte b3 = 014;
+    byte b4 = 0xc;
+    System.out.println(b1 + " " + b2 + " " + b3 + " " + b4);
+
+    short s1 = 1300;
+    short s2 = 0b10100010100;
+    short s3 = 02424;
+    short s4 = 0x514;
+    System.out.println(s1 + " " + s2 + " " + s3 + " " + s4);
+
+    int i1 = 0;
+    int i2 = 0b0;
+    int i3 = 00;
+    int i4 = 0x0;
+    System.out.println(i1 + " " + i2 + " " + i3 + " " + i4);
+
+    // long
+  }
 }
