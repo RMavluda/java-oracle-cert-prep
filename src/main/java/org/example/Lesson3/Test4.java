36bdc17edcb21693eee6584b1a78d31c0e60b55a
mavluda <m.raximquliyeva@uzinfocom.uz>
2025-02-13 12:06:00 +0500

    Test4

--- src/main/java/org/example/Lesson3/Test4.java
+++ src/main/java/org/example/Lesson3/Test4.java
@@ -3,19 +3,39 @@ package org.example.Lesson3;
 public class Test4 {
 
   public static void main(String[] args) {
+
+/*
+Unary operations
+++ prefix
+-- postfix
+*/
+    int x = 5;
+    int y = 3;
+    int z = x - ++y;
+    System.out.println("z = " + z);
+    System.out.println("y = " + y);
+    int a = 5;
+    int b = ++a - --a - a-- + a++;
+    System.out.println("b = " + b);
+    System.out.println("a = " + a);
+
+/*
     double a = 5.5;
     double b = 3.5;
 
     double c = a % b;
     System.out.println(c);
+*/
 
-//    int a = 11;
-//    int b = 3;
-//
-//    int wholePart = a / b;
-//    int remainder = a % b;
-//
-//    System.out.println(wholePart);
-//    System.out.println(remainder);
+/*
+    int a = 11;
+    int b = 3;
+
+    int wholePart = a / b;
+    int remainder = a % b;
+
+    System.out.println(wholePart);
+    System.out.println(remainder);
+*/
   }
 }
