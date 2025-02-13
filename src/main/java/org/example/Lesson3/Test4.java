6f957b8e9586fc63e965ddca7b77a52631295568
mavluda <m.raximquliyeva@uzinfocom.uz>
2025-02-13 11:53:36 +0500

    Test4

---
+++ src/main/java/org/example/Lesson3/Test4.java
@@ -0,0 +1,21 @@
+package org.example.Lesson3;
+
+public class Test4 {
+
+  public static void main(String[] args) {
+    double a = 5.5;
+    double b = 3.5;
+
+    double c = a % b;
+    System.out.println(c);
+
+//    int a = 11;
+//    int b = 3;
+//
+//    int wholePart = a / b;
+//    int remainder = a % b;
+//
+//    System.out.println(wholePart);
+//    System.out.println(remainder);
+  }
+}
