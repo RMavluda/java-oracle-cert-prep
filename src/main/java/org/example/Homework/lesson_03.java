6f89e6cc86bffaf4f80e6f8a43fcfb757ca64354
mavluda <rahimqulimavluda@gmail.com>
2025-02-24 22:47:42 +0500

    Homework lesson 3

--- src/main/java/org/example/Homework/lesson_03.java
+++ src/main/java/org/example/Homework/lesson_03.java
@@ -1,12 +1,25 @@
 package org.example.Homework;
 
 public class lesson_03 {
-public static void main(String [] args){
 
-/*  boolean b1 = false;
-  boolean b2 =      b1 == true;
-  System.out.println(b2); // false
-  boolean b3 = b2 = true;
-  System.out.println(b3);*/
-}
+  public static void main(String[] args) {
+    System.out.println("task 1:");
+    int i1 = 5;
+    int i2 = 11;
+    double d1 = 5.5;
+    double d2 = 1.3;
+    long l = 20l;
+    double result = 0;
+    result = i2 / d1 + d2 % i1 - l;
+    System.out.println(i2 / d1);
+    System.out.println(d2 % i1);
+    System.out.println(result);
+    System.out.println("task 2a:");
+    int a = 5;
+    a = a-- - --a + ++a + a++ + a;
+    System.out.println(a);
+    int b = 8;
+    b = ++b - b++ + ++b - --b;
+    System.out.println(b);
+  }
 }
