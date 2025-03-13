3ab9ace0bec2c49eccd20a5a60c70da8e229b31d
mavluda <rahimqulimavluda@gmail.com>
2025-03-13 11:56:39 +0500

    lesson 5

--- src/main/java/org/example/Lesson05/Test20.java
+++ src/main/java/org/example/Lesson05/Test20.java
@@ -3,8 +3,11 @@ package org.example.Lesson05;
 public class Test20 {
 
   int sum(int a, int b, int c) { //parameters or arguments
-    int result = a + b + c;
-    return result;
+    return a + b + c;
+  }
+
+  int arithmeticMean(int a1, int b1, int c1){
+    return sum(a1, b1, c1) / 3;
   }
 }
 
@@ -14,5 +17,6 @@ class Test21{
     int sumOfThreeNumbers = t.sum(1, 2, 3);
     System.out.println(sumOfThreeNumbers);
     System.out.println(t.sum(15, 23, -1));
+    System.out.println(t.arithmeticMean(20, 40, 60));
   }
 }
