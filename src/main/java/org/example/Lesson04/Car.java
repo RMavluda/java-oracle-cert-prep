0b75ebda183297331a7d517d8fdc79498869ac7c
mavluda <rahimqulimavluda@gmail.com>
2025-03-13 12:10:42 +0500

    constructor

--- src/main/java/org/example/Lesson04/Car.java
+++ src/main/java/org/example/Lesson04/Car.java
@@ -2,21 +2,31 @@ package org.example.Lesson04;
 
 public class Car {
 
-  String color = "red";
-  String engine = "v6";
+  Car(String svet, String motor) {
+    color = svet;
+    engine = motor;
+
+  }
+
+  String color;
+  String engine;
 }
 
 class carTest {
 
   public static void main(String[] args) {
     int a;
-    Car car1 = new Car();
+    Car car1 = new Car("yellow", "V4");
 //    Car car2 = new Car();
 //    Car car2 = car1;
 //    Car car3;
 
+    System.out.println(car1.color);
+    System.out.println(car1.engine);
+
     car1.color = "black";
     car1.engine = "V8";
+
     System.out.println("Color: " + car1.color);
     System.out.println("Engine: " + car1.engine);
 
