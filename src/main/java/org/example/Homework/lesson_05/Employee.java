1a00aa94c236f802a17acb4fb1303f08c070795f
mavluda <rahimqulimavluda@gmail.com>
2025-03-14 14:34:57 +0500

    homework 5

--- src/main/java/org/example/Homework/lesson_05/Employee.java
+++ src/main/java/org/example/Homework/lesson_05/Employee.java
@@ -2,6 +2,12 @@ package org.example.Homework.lesson_05;
 
 public class Employee {
 
+  int id;
+  String surname;
+  int age;
+  int salary;
+  String department;
+
   Employee(int id1, String surname1, int age1, int salary1, String department1) {
     id = id1;
     surname = surname1;
@@ -10,14 +16,9 @@ public class Employee {
     department = department1;
   }
 
-  int id;
-  String surname;
-  int age;
-  int salary;
-  String department;
-
-  void salaryIncrease() {
+  double salaryIncrease() {
     salary *= 2;
+    return salary;
   }
 }
 
@@ -27,8 +28,8 @@ class EmployeeTest {
     Employee employee1 = new Employee(1, "John", 26, 1200, "Tech Support");
     Employee employee2 = new Employee(2, "Maria", 26, 2500, "Management");
     employee1.salaryIncrease();
+    System.out.println(employee1.surname + "`s salary: " + employee1.salary);
     employee2.salaryIncrease();
-    System.out.println("John`s salary: " + employee1.salary);
-    System.out.println("Maria`s salary: " + employee2.salary);
+    System.out.println(employee2.surname + "`s salary: " + employee2.salary);
   }
 }
