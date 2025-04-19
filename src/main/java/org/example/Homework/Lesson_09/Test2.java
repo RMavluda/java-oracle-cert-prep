226bf3fe6c9b0deeb4cad912e7387520c1bb2e01
mavluda <rahimqulimavluda@gmail.com>
2025-04-19 17:47:40 +0500

    lesson 9 Homework task2 answer

--- src/main/java/org/example/Homework/Lesson_09/Test2.java
+++ src/main/java/org/example/Homework/Lesson_09/Test2.java
@@ -4,8 +4,8 @@ public class Test2 {
 int a = 1;
 static int b = 2;
 static void abc(final int a){
-  System.out.println(a);
-  System.out.println(Test2.b);;
+  System.out.println(a); // shunchaki 2 yozilsa parametrdagi 2 olinadi
+  System.out.println(Test2.b);
 }
 
   public static void main(String[] args) {
