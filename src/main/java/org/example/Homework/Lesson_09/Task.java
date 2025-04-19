d69a13b68e3222686601b9dc51316719244d1795
mavluda <rahimqulimavluda@gmail.com>
2025-04-19 17:41:27 +0500

    lesson 9 Homework task1 answer

--- src/main/java/org/example/Homework/Lesson_09/Task.java
+++ src/main/java/org/example/Homework/Lesson_09/Task.java
@@ -1,26 +1,16 @@
 package org.example.Homework.Lesson_09;
 
 public class Task {
-
+public static void abc(){
+  String s1 = new String("ABC");
+  String s2 = new String("DEF");
 }
 
-class TasTest {
-
   public static void main(String[] args) {
-
-    Task t1 = new Task();
-    Task t2 = new Task();
-    Task t3 = new Task();
-    Task t4 = new Task();
-    Task t5 = new Task();
-    Task t6 = new Task();
-    Task t7 = new Task();
-    Task t8 = new Task();
-    t3 = null;
-    t4 = null;
-    t5 = null;
-    t6 = null;
-    t7 = null;
-    t8 = null;
+    Task t = new Task();
+    abc();
+    abc();
+    String s1 = new String("ABC");
+    abc();
   }
 }
