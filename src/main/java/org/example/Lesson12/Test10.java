3b73138ac3a70445033a5f404f52bc5a1cdaf271
mavluda <rahimqulimavluda@gmail.com>
2025-04-23 18:16:59 +0500

    lesson 12

--- src/main/java/org/example/Lesson12/Test10.java
+++ src/main/java/org/example/Lesson12/Test10.java
@@ -8,7 +8,7 @@ public class Test10 {
 
     if (salary < 200) {
       System.out.println("Z/P ochen nizka");
-    } else if (salary < 400) {
+    } else if (salary < 400 && b == false || true ) {
       System.out.println("Z/P srednego razmera");
     } else if (salary < 600) {
       System.out.println("Z/P visokaya");
