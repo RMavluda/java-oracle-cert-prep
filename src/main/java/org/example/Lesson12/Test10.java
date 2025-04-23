e317727a0b973316b25dc152dcb0f05fac05162b
mavluda <rahimqulimavluda@gmail.com>
2025-04-23 18:01:05 +0500

    lesson 12 if else

--- src/main/java/org/example/Lesson12/Test10.java
+++ src/main/java/org/example/Lesson12/Test10.java
@@ -1,5 +1,6 @@
 package org.example.Lesson12;
 
+
 public class Test10 {
 
   public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class Test10 {
       System.out.println("1: a < 20");
       System.out.println("ajdshdshds");
       a++;
-    //agar figurali qovus yozilmasa if faqat amalni hisobga oladi
+    //agar figurali qavs yozilmasa if faqat amalni hisobga oladi
     //ammo undan keyingi amallar ham bajarialdi
 
 
@@ -20,7 +21,7 @@ public class Test10 {
      System.out.println("Hello everyone!");
 
     //else da ham huddi shunday
-    //figurali qovus qoyilmasa, faqat bittagina amal else ga kiradi
+    //figurali qavs qoyilmasa, faqat bittagina amal else ga kiradi
 
     int salary = 500;
     if (salary >= 500) {
@@ -36,5 +37,15 @@ public class Test10 {
     if (salary > 500)
     System.out.println("abrakadabra 100");
     //sout if ichida hisoblanadi
+
+/*    if (salary >= 500)
+    System.out.println(":)))");
+    System.out.println("!!!!!!!");
+    else
+      System.out.println(":(");
+
+      ERROR chunki figurali qavs ishaltilmagan va else 2-virajeniyadan keyin yozilyapti'
+      ya`ni bu ko`rinishda kompilyator else uchun if yozilmagan deb hisoblaydi
+      */
   }
 }
