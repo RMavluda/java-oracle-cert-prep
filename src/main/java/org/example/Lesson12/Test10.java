19b20815931230beda3e031f7d9b03f8b9099f32
mavluda <rahimqulimavluda@gmail.com>
2025-04-23 17:12:44 +0500

    lesson 12

--- src/main/java/org/example/Lesson12/Test10.java
+++ src/main/java/org/example/Lesson12/Test10.java
@@ -4,15 +4,19 @@ public class Test10 {
 
   public static void main(String[] args) {
 
-    int a = 10;
+    int a = 30;
     if(a < 20)
-      System.out.println("a < 20");
+      System.out.println("1: a < 20");
       System.out.println("ajdshdshds");
       a++;
-
-
-
     //agar figurali qovus yozilmasa if faqat amalni hisobga oladi
     //ammo undan keyingi amallar ham bajarialdi
+
+
+   if(a < 20){
+     System.out.println("2: a < 20");
+   }else{
+     System.out.println("a > 20");
+   }
   }
 }
