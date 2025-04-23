de907a78bbd30b2e2aca9dcd8b867d5ecab01d57
mavluda <rahimqulimavluda@gmail.com>
2025-04-23 16:57:33 +0500

    lesson 11 Homework

--- src/main/java/org/example/Homework/Lesson_11/Car.java
+++ src/main/java/org/example/Homework/Lesson_11/Car.java
@@ -4,7 +4,7 @@ public class Car {
 
   String color;
   String engine;
-  public static int doorsCount;
+  int doorsCount;
 
   Car(String color, String engine) {
     this.color = color;
@@ -13,19 +13,23 @@ public class Car {
 }
 
 class CarTest {
-  void changeDoorsCount(int count){
-    Car.doorsCount = count;
+  void changeDoorsCount(Car c, int count){
+    c.doorsCount = count;
   }
 
-  void swap(Car c1, Car c2){
+  void changeColor(Car c1, Car c2){
      String temp = c1.color;
     c1.color = c2.color;
     c2.color = temp;
   }
 
   public static void main(String[] args) {
+    CarTest ct = new CarTest();
     Car c1 = new Car("White", "Ford");
     Car c2 = new Car("Black", "Mercedes-Benz");
-    
+    ct.changeDoorsCount(c1, 3);
+    ct.changeColor(c1, c2);
+    System.out.println("Car1: color: " + c1.color + ", engine: " + c1.engine + ", doorsCount: " + c1.doorsCount);
+    System.out.println("Car2: color: " + c2.color + ", engine: " + c2.engine + ", doorsCount: " + c2.doorsCount);
   }
 }
