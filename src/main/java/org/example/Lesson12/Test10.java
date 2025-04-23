c64a963ad7ab46e34982febe246891be6509d242
mavluda <rahimqulimavluda@gmail.com>
2025-04-23 17:10:46 +0500

    lesson 12

---
+++ src/main/java/org/example/Lesson12/Test10.java
@@ -0,0 +1,18 @@
+package org.example.Lesson12;
+
+public class Test10 {
+
+  public static void main(String[] args) {
+
+    int a = 10;
+    if(a < 20)
+      System.out.println("a < 20");
+      System.out.println("ajdshdshds");
+      a++;
+
+
+
+    //agar figurali qovus yozilmasa if faqat amalni hisobga oladi
+    //ammo undan keyingi amallar ham bajarialdi
+  }
+}
