0e27f2ce66095425ca6a92505f24fcb3d7b475e0
mavluda <rahimqulimavluda@gmail.com>
2025-04-23 18:35:46 +0500

    lesson 12

---
+++ src/main/java/org/example/Lesson12/Test10_2.java
@@ -0,0 +1,34 @@
+package org.example.Lesson12;
+
+public class Test10_2 {
+void maximum(int i1, int i2, int i3) {
+  if(i1 > i2 && i1 > i3){
+    int a = 5;
+    System.out.println("Maximum: " + i1);
+  } else if(i2 > i1 && i2 > i3){
+    int a = 10;
+    System.out.println("Maximum: " + i2);
+  } else {
+    int a = 15;
+    System.out.println("Maximum: " + i3);
+  }
+
+  void abc(){
+    String str;
+    int a = 10;
+    if(a >= 10){
+      str = "Hello";
+    }
+    if(a < 1){
+      str = "Bye";
+    }
+    else{
+      str = "adhasbddh";
+    }
+    System.out.println(str);
+  }
+}
+  public static void main(String[] args) {
+
+  }
+}
