016f338ef9200a5b56fda1f7eb8747bff0ce6fff
mavluda <rahimqulimavluda@gmail.com>
2025-04-25 11:50:35 +0500

    lesson 13 Homework

--- src/main/java/org/example/Homework/Lesson_13/Lesson13.java
+++ src/main/java/org/example/Homework/Lesson_13/Lesson13.java
@@ -1,12 +1,13 @@
 package org.example.Homework.Lesson_13;
 
-public class Month {
+public class Lesson13 {
 
-  public static void main(String[] args) {
+}
+class Month {
 
-    int month = 9;
+  public static void dayQuantity(int n) {
 
-    switch (month) {
+    switch (n) {
       case 1:
       case 3:
       case 5:
@@ -28,5 +29,13 @@ public class Month {
       default:
         System.out.println("A month with such a serial number does not exist");
     }
+  }
+
+  public static void main(String[] args) {
+    Month.dayQuantity(0);
+    dayQuantity(1);
+    Month.dayQuantity(2);
+    dayQuantity(4);
+    Month.dayQuantity(-5);
   }
 }
