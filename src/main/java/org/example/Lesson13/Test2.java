2e21cbf10f6a9cae886d090a3e858573c3adb84d
mavluda <rahimqulimavluda@gmail.com>
2025-04-25 11:39:09 +0500

    lesson 13 types switch expression

--- src/main/java/org/example/Lesson13/Test2.java
+++ src/main/java/org/example/Lesson13/Test2.java
@@ -3,22 +3,34 @@ package org.example.Lesson13;
 public class Test2 {
 
   public static void main(String[] args) {
-    switch("Wednesday"){
-      case "Monday":
-      case "Tuesday":
-      case "Wednesday":
-      case "Thursday":
-      case "Friday":
+    short denNedeli = 3;
+    final int a;
+    final int b;
+
+    switch (denNedeli) {
+      case 1:
+      case 2:
+      case 3:
+//case 200: kabi yozish mumkin emas, chunki case da yozilgan type switch parametridagi type bn bir xil bo`lishi shart
+      case 4:
+      case 5:
         System.out.println("Rabota do 18:00");
         break;
-      case "Saturday":
+      case 6:
         System.out.println("Rabota do 14:00");
         break;
-      case "Sunday":
+      case 7:
         System.out.println("Raboti net");
         break;
       default:
         System.out.println("Takogo dnya ne sushestvuyet");
+    }
+    switch ("privet") {
+      case "asadsd":
+        System.out.println("wrong");
+// case null: kabi yozish mumkin emas, null bu yo`qlikdir. ya`ni string hech qanday adresga murojat qilmayapti degani
+      default:
+        System.out.println("not found");
     }
 /*    long l1 = 5L;
     switch (l1){
@@ -32,12 +44,25 @@ public class Test2 {
 }
 
 /*
-* switch(expression)
-* expression bo`lishi mumkin:
-* -byte
-* -short
-* -int
-* -char
-* -String
-*
-* boshqa typelarni qabul qilmidi*/
+ * switch(expression)
+ * expression bo`lishi mumkin:
+ * -byte
+ * -short
+ * -int
+ * -char
+ * -String
+ *
+ * boshqa typelarni qabul qilmidi*/
+
+/*
+ * compile time constants ga kiradi:
+ * 1, -3.14, "privet*, 5*10 kabilar
+ *
+ * !!! compile time constants ga kirmaydi:
+ * a = 5, b = 10, a * b kabilar
+ *
+ * final a = 5, final b = 10, a * b qilish orqali buni to`g`irlash mumkin
+ *
+ * !!! lekin
+ * final a, final b, a = 5, b = 10, a*b compile time constants hisoblanmidi
+ * */
