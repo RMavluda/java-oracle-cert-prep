8644d52a5e7775ec38a8a5e3b01ec495747a43fe
mavluda <rahimqulimavluda@gmail.com>
2025-04-28 16:28:42 +0500

    lesson 14 loop for

--- src/main/java/org/example/Lesson14/Test2.java
+++ src/main/java/org/example/Lesson14/Test2.java
@@ -3,12 +3,17 @@ package org.example.Lesson14;
 public class Test2 {
 
   public static void main(String[] args) {
-    for (int i = 5, j = 10; i < 11 && i > 2 && i > -4; i++, j = j + 20, abc(10)) {
+//    for (int i = 5, j = 10; i < 11 && i > 2 && i > -4; i++, j = j + 20, abc(10)) {
+//      System.out.println(i);
+//    }
+//    System.out.println("\nloop 2:\n");
+//    for (int i = 5; i < 11; i++, abc(10)) {
+//      System.out.println(i);
+//    }
+//    System.out.println("\nloop 3:\n");
+    for (int i = 5; i < 11; i++) {
       System.out.println(i);
-    }
-    System.out.println("\nloop 2:\n");
-    for (int i = 5; i < 11; i++, abc(10)) {
-      System.out.println(i);
+      abc(10);
     }
   }
 
