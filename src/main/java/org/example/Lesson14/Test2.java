bc693a620064852c9f7e23f7302005a7ff994fbb
mavluda <rahimqulimavluda@gmail.com>
2025-04-28 16:37:45 +0500

    lesson 14 loop for

--- src/main/java/org/example/Lesson14/Test2.java
+++ src/main/java/org/example/Lesson14/Test2.java
@@ -3,15 +3,25 @@ package org.example.Lesson14;
 public class Test2 {
 
   public static void main(String[] args) {
-//    for (int i = 5, j = 10; i < 11 && i > 2 && i > -4; i++, j = j + 20, abc(10)) {
-//      System.out.println(i);
-//    }
-//    System.out.println("\nloop 2:\n");
-//    for (int i = 5; i < 11; i++, abc(10)) {
-//      System.out.println(i);
-//    }
-//    System.out.println("\nloop 3:\n");
+    for (int i = 5, j = 10; i < 11 && i > 2 && i > -4; i++, j = j + 20, abc(10)) {
+      System.out.println(i);
+    }
+    System.out.println("\nloop 2:\n");
+    for (int i = 5; i < 11; i++, abc(10)) {
+      System.out.println(i);
+    }
+    System.out.println("\nloop 3:\n");
     for (int i = 5; i < 11; i++) {
+      System.out.println(i);
+      abc(10);
+    }
+//    System.out.println("\nloop 4 => infinitive:\n");
+//    for (int i = 5; i < 11;) {
+//      System.out.println(i);
+//      abc(10);
+//    }
+    System.out.println("\nloop 5 without condition statement => infinitive:\n");
+    for (int i = 5; ;i++) {
       System.out.println(i);
       abc(10);
     }
