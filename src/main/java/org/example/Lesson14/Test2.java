abf80936464ba83bb109bc9636d3e3174047dade
mavluda <rahimqulimavluda@gmail.com>
2025-04-28 16:26:19 +0500

    lesson 14 loop for

--- src/main/java/org/example/Lesson14/Test2.java
+++ src/main/java/org/example/Lesson14/Test2.java
@@ -6,8 +6,13 @@ public class Test2 {
     for (int i = 5, j = 10; i < 11 && i > 2 && i > -4; i++, j = j + 20, abc(10)) {
       System.out.println(i);
     }
+    System.out.println("\nloop 2:\n");
+    for (int i = 5; i < 11; i++, abc(10)) {
+      System.out.println(i);
+    }
   }
-  static void abc(int n){
+
+  static void abc(int n) {
     System.out.println(n);
   }
 }
