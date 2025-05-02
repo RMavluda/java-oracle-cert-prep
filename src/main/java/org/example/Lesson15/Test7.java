a991f582171b82e475cc8e30f9b22c31991339e4
mavluda <rahimqulimavluda@gmail.com>
2025-05-02 09:42:08 +0500

    lesson 15 do while

---
+++ src/main/java/org/example/Lesson15/Test7.java
@@ -0,0 +1,13 @@
+package org.example.Lesson15;
+
+public class Test7 {
+
+  public static void main(String[] args) {
+    int money = 100;
+    do {
+      System.out.println("delayte stavku");
+      System.out.println("vi proigrali");
+      money -= 10;
+    } while (money > 50);
+  }
+}
