d59a847770bbdfc9da73d26bf590734c77c054a9
mavluda <rahimqulimavluda@gmail.com>
2025-05-02 09:46:32 +0500

    lesson 15 infinitive loop

---
+++ src/main/java/org/example/Lesson15/Test8.java
@@ -0,0 +1,20 @@
+package org.example.Lesson15;
+
+public class Test8 {
+
+  public static void main(String[] args) {
+ /*   do{
+      System.out.println("dskfjdsfn");
+    }while(true); // infinitive loop*/
+
+    do {
+      System.out.println("privet");
+    } while (5 == 10);
+
+    while (10 == 10) {  // infinitive loop
+      System.out.println("10 = 10");
+    }
+    //System.out.println("poka");
+  }
+
+}
