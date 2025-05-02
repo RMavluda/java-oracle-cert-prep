4f41df11a049d758c8990584743b0a3e9b76ee77
mavluda <rahimqulimavluda@gmail.com>
2025-05-02 09:26:44 +0500

    lesson 15 test4

---
+++ src/main/java/org/example/Lesson15/Test4.java
@@ -0,0 +1,13 @@
+package org.example.Lesson15;
+
+public class Test4 {
+
+  public static void main(String[] args) {
+    int a = 5;
+    while (a++ < 10) {
+      System.out.print(a + " ");
+      a++;
+      System.out.println(a);
+    }
+  }
+}
