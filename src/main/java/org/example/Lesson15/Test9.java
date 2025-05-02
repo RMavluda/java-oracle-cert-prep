b29a39bea4a7994f40c51c09d82c068d73e906a7
mavluda <rahimqulimavluda@gmail.com>
2025-05-02 09:59:12 +0500

    lesson 15

--- src/main/java/org/example/Lesson15/Test9.java
+++ src/main/java/org/example/Lesson15/Test9.java
@@ -3,16 +3,28 @@ package org.example.Lesson15;
 public class Test9 {
 
   public static void main(String[] args) {
-    int chas = 0;
+    int chas = -1;
     OUTER:
     do {
+      chas++;
       int minuta = 0;
       INNER:
       while (minuta < 60) {
+        if (minuta == 20) {
+          break OUTER;
+        }
         System.out.println(chas + ":" + minuta);
         minuta++;
       }
-      chas++;
-    }while(chas < 24);
+    } while (chas < 24);
+
   }
 }
+/*
+    do {
+      int a = 5;
+      System.out.println(a);
+    }while(a <= 15);
+
+    bu sikl error bo`ladi, chunki a o`zgaruvchini while tanimaydi,
+    a do ning ichida yaratilgan, undan tashqarida mavjud emas*/
