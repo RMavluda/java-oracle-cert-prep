5cd8e8107a0f48845bdddc87bdfcc028b6a32855
mavluda <rahimqulimavluda@gmail.com>
2025-05-02 09:36:31 +0500

    lesson 15 test5

---
+++ src/main/java/org/example/Lesson15/Test5.java
@@ -0,0 +1,11 @@
+package org.example.Lesson15;
+
+public class Test5 {
+
+  public static void main(String[] args) {
+    int a = 5;
+    while (a < 10);
+    System.out.println("privet");
+
+  }
+}
