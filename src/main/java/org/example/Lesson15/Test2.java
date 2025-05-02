167a1e8bcf1d2a3b44e1e50d2267665ecf093a21
mavluda <rahimqulimavluda@gmail.com>
2025-05-02 09:16:50 +0500

    lesson 15 test2

---
+++ src/main/java/org/example/Lesson15/Test2.java
@@ -0,0 +1,14 @@
+package org.example.Lesson15;
+
+public class Test2 {
+
+  public static void main(String[] args) {
+    int money = 100;
+    while(money > 0){
+      System.out.println("Delayte stavku");
+      System.out.println("Vi proigrali");
+      money = money - 10;
+    }
+  }
+
+}
