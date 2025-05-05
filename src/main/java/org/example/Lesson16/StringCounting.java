d1f3f9732bacb645e3384eb384f7445cb1ce0bc0
mavluda <rahimqulimavluda@gmail.com>
2025-05-05 10:25:53 +0500

    lesson 16

---
+++ src/main/java/org/example/Lesson16/StringCounting.java
@@ -0,0 +1,14 @@
+package org.example.Lesson16;
+
+public class StringCounting {
+
+  public static void main(String[] args) {
+    String s1 = new String("Hello");
+    String s2 = "Hello";
+    System.out.println("Hello");
+    System.out.println("Bye");
+    System.out.println("Bye" == "hello");
+    String autumn = new String("Hello");
+  }
+
+}
