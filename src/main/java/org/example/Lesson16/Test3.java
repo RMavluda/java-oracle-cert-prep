1ac1aa072175554a6cad22d18f45d9166340f742
mavluda <rahimqulimavluda@gmail.com>
2025-05-05 12:15:35 +0500

    lesson 16

---
+++ src/main/java/org/example/Lesson16/Test3.java
@@ -0,0 +1,19 @@
+package org.example.Lesson16;
+
+public class Test3 {
+
+  public static void main(String[] args) {
+
+    int a = 5;
+    int b = 6;
+    String s = "ok";
+    System.out.println(a + b + s);
+    System.out.println("" + a + b + s);
+    System.out.println("" + (a + b) + s);
+
+    String s1 = "hello";
+//    String s2 = a+8;
+//    System.out.println(s2);
+  }
+
+}
