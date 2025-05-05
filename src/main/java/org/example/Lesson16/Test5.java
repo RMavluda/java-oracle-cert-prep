a147416dc989f7fa15e0069a793443dedd085595
mavluda <rahimqulimavluda@gmail.com>
2025-05-05 12:23:46 +0500

    lesson 16

---
+++ src/main/java/org/example/Lesson16/Test5.java
@@ -0,0 +1,17 @@
+package org.example.Lesson16;
+
+public class Test5 {
+
+  public static void main(String[] args) {
+    String s = null;
+    s += "ok";
+    System.out.println(s);
+//    System.out.println(null + true);
+  /*
+  * 1-type: nulltype
+  * 2-type: boolean
+  * ularni qo`shish mumkin emas*/
+
+
+  }
+}
