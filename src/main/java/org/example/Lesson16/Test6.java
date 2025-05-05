d36f746a0b9aa35967b0664dd8bdf1dc7ce3c8b0
mavluda <rahimqulimavluda@gmail.com>
2025-05-05 14:07:46 +0500

    lesson 16 Test6.java

---
+++ src/main/java/org/example/Lesson16/Test6.java
@@ -0,0 +1,17 @@
+package org.example.Lesson16;
+
+public class Test6 {
+
+  public static void main(String[] args) {
+    String s = "PriVET: 2!";
+    String s2 = s.toLowerCase();
+    System.out.println(s2);
+
+    String s3 = s.toUpperCase();
+    System.out.println(s3);
+
+    boolean b = s.contains("t: 2!");
+    System.out.println(b);
+  }
+
+}
