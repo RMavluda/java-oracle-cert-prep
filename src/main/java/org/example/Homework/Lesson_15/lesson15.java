94180de817a55375432f9a5f50fb2a14b7c04a39
mavluda <rahimqulimavluda@gmail.com>
2025-05-05 08:36:50 +0500

    lesson 15 homework

--- src/main/java/org/example/Homework/Lesson_15/lesson15.java
+++ src/main/java/org/example/Homework/Lesson_15/lesson15.java
@@ -5,24 +5,24 @@ public class lesson15 {
   static void clock() {
     int chas = 0;
     OUTLINE:
-    while(chas < 6){
+    while (chas < 6) {
       int minuta = -1;
       MIDDLE:
-      do{
+      do {
         minuta++;
         if (chas > 1 && minuta % 10 == 0) {
           break OUTLINE;
         }
         int sekunda = 0;
         INNER:
-        while(sekunda < 60){
+        while (sekunda < 60) {
           if (sekunda * chas > minuta) {
             continue INNER;
           }
           System.out.println(chas + ":" + minuta + ":" + sekunda);
           sekunda++;
         }
-      }while(minuta < 59);
+      } while (minuta < 59);
       chas++;
     }
   }
