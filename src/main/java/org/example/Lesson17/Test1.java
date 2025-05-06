674841f521aa71f913f0a437421ec6b0ec6ef98f
mavluda <rahimqulimavluda@gmail.com>
2025-05-06 18:09:23 +0500

    lesson 17 StringBuilder

--- src/main/java/org/example/Lesson17/Test1.java
+++ src/main/java/org/example/Lesson17/Test1.java
@@ -1,5 +1,9 @@
 package org.example.Lesson17;
-class Car{}
+
+class Car {
+
+}
+
 public class Test1 {
 
   public static void main(String[] args) {
@@ -18,15 +22,23 @@ public class Test1 {
     System.out.println(s2);
     System.out.println(sb2.subSequence(5, 8));
 
-    sb2.append(22);
-    System.out.println(sb2);
-    sb2.append(true);
-    System.out.println(sb2);
-    sb2.append(sb2);
-    System.out.println(sb2);
-    sb2.append("Hello");
-    System.out.println(sb2);
-    sb2.append(new Car());
-    System.out.println(sb2);
+//    sb2.append(22);
+//    System.out.println(sb2);
+//    sb2.append(true);
+//    System.out.println(sb2);
+//    sb2.append(sb2);
+//    System.out.println(sb2);
+//    sb2.append("Hello");
+//    System.out.println(sb2);
+//    sb2.append(new Car());
+//    System.out.println(sb2);
+
+    System.out.println(sb2.insert(4, 55));
+    System.out.println(sb2.insert(sb2.length(), "Hello"));
+
+    StringBuilder sb10 = new StringBuilder("Hello World");
+    sb10.delete(3, 6);
+    System.out.println(sb10);
+
   }
 }
