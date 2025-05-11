efdf995586e7fc4e0fe508c94666a5081a92c46b
mavluda <rahimqulimavluda@gmail.com>
2025-05-11 11:15:10 +0500

    lesson 17 StringBuilder

--- src/main/java/org/example/Lesson17/Test2.java
+++ src/main/java/org/example/Lesson17/Test2.java
@@ -3,9 +3,10 @@ package org.example.Lesson17;
 public class Test2 {
 
   public static void main(String[] args) {
-    StringBuilder sb1 = new StringBuilder("Hello, friend!");
-    String s = null;
-    s = sb1.append("How are you?").substring(sb1.indexOf("f"), sb1.indexOf("!"));
-    System.out.println(s);
+    StringBuilder sb1 = new StringBuilder("123");
+    StringBuilder sb2 = sb1.append("45");
+    sb2 = sb2.append("6").append("7");
+    System.out.println("sb1 = " + sb1);
+    System.out.println("sb2 = " + sb2);
   }
 }
