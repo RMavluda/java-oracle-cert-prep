1f96ef9d34db00f4a97c22fc004a322ff757849e
mavluda <rahimqulimavluda@gmail.com>
2025-05-11 11:32:18 +0500

    lesson 17 Homework

--- src/main/java/org/example/Homework/Lesson_17.java
+++ src/main/java/org/example/Homework/Lesson_17.java
@@ -2,13 +2,29 @@ package org.example.Homework;
 
 public class Lesson_17 {
 
-  public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2){
-    return sb1.equals(sb2);
+  public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2) {
+    boolean result = true;
+    if (sb1.length() == sb2.length()) {
+      for (int i = 0; i < sb1.length(); i++) {
+        if (sb1.charAt(i) != sb2.charAt(i)) {
+          result = false;
+          break;
+        }
+      }
+    } else {
+      result = false;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
-    StringBuilder sb1 = new StringBuilder("Hb12");
-    StringBuilder sb2 = new StringBuilder("Hb12");
-    System.out.print(ravenstvo(sb1, sb2));
+    StringBuilder sb1 = new StringBuilder("hello");
+    StringBuilder sb2 = new StringBuilder("hello");
+    StringBuilder sb3 = new StringBuilder("Hello");
+    boolean a = Lesson_17.ravenstvo(sb1, sb2);
+    System.out.println(a);
+    System.out.println(Lesson_17.ravenstvo(sb2, sb3));
+    System.out.println(Lesson_17.ravenstvo(new StringBuilder(""), new StringBuilder("")));
+    System.out.println(Lesson_17.ravenstvo(new StringBuilder(), new StringBuilder()));
   }
 }
