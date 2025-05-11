70627973176be57f53144cfcef7f6a90c18def4e
mavluda <rahimqulimavluda@gmail.com>
2025-05-11 11:09:31 +0500

    lesson 17 StringBuilder

---
+++ src/main/java/org/example/Lesson17/Test2.java
@@ -0,0 +1,11 @@
+package org.example.Lesson17;
+
+public class Test2 {
+
+  public static void main(String[] args) {
+    StringBuilder sb1 = new StringBuilder("Hello, friend!");
+    String s = null;
+    s = sb1.append("How are you?").substring(sb1.indexOf("f"), sb1.indexOf("!"));
+    System.out.println(s);
+  }
+}
