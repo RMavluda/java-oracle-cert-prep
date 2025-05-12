457130aa85eb952d56c725ddb0c3d5420172d551
mavluda <rahimqulimavluda@gmail.com>
2025-05-12 08:35:57 +0500

    lesson 18 declaration & location & initialization

--- src/main/java/org/example/Lesson18/Test2.java
+++ src/main/java/org/example/Lesson18/Test2.java
@@ -3,13 +3,15 @@ package org.example.Lesson18;
 public class Test2 {
 
   public static void main(String[] args) {
+    //declaration
     String[] array1;
     int[][] array2;
-
+    // location
     array1 = new String[3];
     array2 = new int[3][0];
 
-    for(int i = 0; i < array1.length; i++){
+    for (int i = 0; i < array1.length; i++) {
+      //initialization
       array1[i] = "privet" + i;
       System.out.println(array1[i]);
     }
@@ -18,12 +20,23 @@ public class Test2 {
     array2[1] = new int[2];
     array2[2] = new int[7];
 
-    for(int i = 0; i < array2.length; i++){
-      for(int j = 0; j < array2[i].length; j++){
-        array2[i][j] =  i + j;
+    for (int i = 0; i < array2.length; i++) {
+      for (int j = 0; j < array2[i].length; j++) {
+        array2[i][j] = i + j;
         System.out.println(array2[i][j]);
       }
     }
+    // declaration & location & initialization
+    int[] array3 = {1, 2, 3};
+
+    //declaration
+    int[] array4;
+
+    //location & initialization
+    array4 = new int[]{1, 2, 3};
+
+    // tak nelzya!!!! massiv o`lchami e`lon qilinmasdan unga ma`lumot kiritixh mumkin emas
+    // array4 = {1, 2, 3};
   }
 
 }
