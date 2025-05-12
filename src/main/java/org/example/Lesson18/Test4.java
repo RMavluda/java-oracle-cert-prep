32793c0c2bc3b789871dcb1bd4f5c61d3e728f2d
mavluda <rahimqulimavluda@gmail.com>
2025-05-12 08:54:04 +0500

    lesson 18 exceptions Arrays.sort & Arrays.binarySearch

---
+++ src/main/java/org/example/Lesson18/Test4.java
@@ -0,0 +1,24 @@
+package org.example.Lesson18;
+
+import java.util.Arrays;
+
+public class Test4 {
+
+  public static void main(String[] args) {
+    int array1[] = {1, 9, 3, -8, 0, 5, 4, 1};
+    for (int i = 0; i < array1.length; i++) {
+      System.out.print(array1[i] + " ");
+    }
+    System.out.println();
+    //sort(array)
+    Arrays.sort(array1);
+    for (int i = 0; i < array1.length; i++) {
+      System.out.print(array1[i] + " ");
+    }
+    System.out.println();
+    //binarySearch(array, value)
+    int index1 = Arrays.binarySearch(array1, 5);
+    System.out.println(index1);
+  }
+
+}
