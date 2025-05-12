3c6f859fbe961f7a1fbdee76d3e17e99eb34c0aa
mavluda <rahimqulimavluda@gmail.com>
2025-05-12 08:49:15 +0500

    lesson 18 exceptions

---
+++ src/main/java/org/example/Lesson18/Test3.java
@@ -0,0 +1,22 @@
+package org.example.Lesson18;
+
+public class Test3 {
+
+  static String s;
+  public static void main(String[] args) {
+//    int[] array = new int[-3]; => NegativeArraySizeException
+    int[] array = new int[3];
+    array[0] = 1;
+    array[1] = 2;
+    array[2] = 3;
+//    array[3] = 3; => ArrayIndexOutOfBoundsException
+
+    int [][] array2 = new int[3][];
+//    System.out.println(array2[0][1]); => NullPointerException
+
+//    System.out.println(s.length()); => NullPointerException
+
+
+  }
+
+}
