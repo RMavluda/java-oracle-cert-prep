790a7f5ce9cd6b1e982dfd2dbd4cc896d9ceb84b
mavluda <rahimqulimavluda@gmail.com>
2025-05-13 08:37:12 +0500

    homework 18

---
+++ src/main/java/org/example/Homework/Lesson_18/Task1.java
@@ -0,0 +1,46 @@
+package org.example.Homework.Lesson_18;
+
+public class Task1 {
+
+  public static int[] sortA(int[] array) {
+    for(int i = 0; i < array.length - 1; i++) {
+      for(int j = i + 1; j < array.length; j++){
+        if(array[i] < array[j]){
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+      }
+    }
+    return array;
+  }
+
+  public static int[] sortB(int[] array) {
+    for(int i = 0; i < array.length - 1; i++) {
+      for(int j = i + 1; j < array.length; j++){
+        if(array[i] > array[j]){
+          int temp = array[j];
+          array[j] = array[i];
+          array[i] = temp;
+        }
+      }
+    }
+    return array;
+  }
+
+  public static void showArray(int[] array) {
+    for(int element : array){
+      System.out.print(element + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    int[] array = new int[]{101,-2,32,-41,57,6,-74,80,91};
+    showArray(array);
+    sortA(array);
+    showArray(array);
+    sortB(array);
+    showArray(array);
+  }
+}
