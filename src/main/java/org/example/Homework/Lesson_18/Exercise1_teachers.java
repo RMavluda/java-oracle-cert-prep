8a8b2018ff86fa948fda8ad08ac3659ff658a278
mavluda <rahimqulimavluda@gmail.com>
2025-05-13 09:01:30 +0500

    homework 18 exercise 1 selection sort

--- src/main/java/org/example/Homework/Lesson_18/Exercise1_teachers.java
+++ src/main/java/org/example/Homework/Lesson_18/Exercise1_teachers.java
@@ -22,6 +22,13 @@ public class Exercise1_teachers {
     }
     return array;
   }
+  /*
+  * har bir iteratsiyada eng kichik elementni aniqlidi va iteratsiya boshlangan element bilan o`rnini almashtirib boradi
+  * masalan: {0, 5, -3, 2}
+  * i = 0: iteratsiya 0-elementdan boshlidi, [0:3] oraliqdagi min element indexi 2, 2- va 1- elementlar alamashtiriladi --> {-3, 5, 0, 2}
+  * i = 1: iteratsiya 1-elementdan boshlidi, [1:3] oraliqdagi min element indexi 2, 2- va 1- elementlar alamashtiriladi --> {-3, 0, 5, 2}
+  * i = 2: iteratsiya 2-elementdan boshlidi, [2:3] oraliqdagi min element indexi 3, 3- va 2- elementlar alamashtiriladi --> {-3, 0, 2, 5}
+  * */
 
   public static void main(String[] args) {
     int[] array = new int[]{5, 8, 1, -3, 0, 8, 2, 2};
