862f8e30e917d3a3f2ba6c0a7e9c4a8cd16a87ed
mavluda <rahimqulimavluda@gmail.com>
2025-05-13 08:47:15 +0500

    homework 18 exercise 1

--- src/main/java/org/example/Homework/Lesson_18/Exercise1_my.java
+++ src/main/java/org/example/Homework/Lesson_18/Exercise1_my.java
@@ -3,12 +3,12 @@ package org.example.Homework.Lesson_18;
 public class Exercise1_my {
 
   public static int[] descending(int[] array) {
-    for(int i = 0; i < array.length - 1; i++) {
-      for(int j = i + 1; j < array.length; j++){
-        if(array[i] < array[j]){
-            int temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[i] < array[j]) {
+          int temp = array[j];
+          array[j] = array[i];
+          array[i] = temp;
         }
       }
     }
@@ -16,9 +16,9 @@ public class Exercise1_my {
   }
 
   public static int[] ascending(int[] array) {
-    for(int i = 0; i < array.length - 1; i++) {
-      for(int j = i + 1; j < array.length; j++){
-        if(array[i] > array[j]){
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[i] > array[j]) {
           int temp = array[j];
           array[j] = array[i];
           array[i] = temp;
@@ -29,14 +29,14 @@ public class Exercise1_my {
   }
 
   public static void showArray(int[] array) {
-    for(int element : array){
+    for (int element : array) {
       System.out.print(element + " ");
     }
     System.out.println();
   }
 
   public static void main(String[] args) {
-    int[] array = new int[]{101,-2,32,-41,57,6,-74,80,91};
+    int[] array = new int[]{101, -2, 32, -41, 57, 6, -74, 80, 91};
     showArray(array);
     descending(array);
     showArray(array);
