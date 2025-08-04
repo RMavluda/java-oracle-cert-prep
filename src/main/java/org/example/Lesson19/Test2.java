b196074c913c64ceb019799cdd91ca9a4073a307
mavluda <rahimqulimavluda@gmail.com>
2025-08-04 09:54:59 +0500

    varargs parameter

---
+++ src/main/java/org/example/Lesson19/Test2.java
@@ -0,0 +1,20 @@
+package org.example.Lesson19;
+
+public class Test2 {
+  static void summa(String s, int ... a){
+    int summa = 0;
+    for(int i = 0; i < a.length; i++) {
+      summa += a[i];
+    }
+    System.out.println(s + " " + summa);
+  }
+
+  public static void main(String[] args) {
+    summa("ok",1,2,3,4,5,6,7,8,9);
+    summa("done", new int []{3, 5});
+  }
+}
+/*
+* varargs parametr dan keyin boshqa parametr berilishi mumkin emas
+* faqat undan oldin kiritish mumkin bo`ladi
+* */
