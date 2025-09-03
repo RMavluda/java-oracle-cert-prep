d3e69807755774b22c121780f5b567dcec132270
mavluda <rahimqulimavluda@gmail.com>
2025-09-03 11:17:40 +0500

    ArrayList

---
+++ src/main/java/org/example/Lesson20/Test2.java
@@ -0,0 +1,22 @@
+package org.example.Lesson20;
+
+import java.util.ArrayList;
+
+public class Test2 {
+
+  public static void main(String[] args) {
+
+    ArrayList<String> list = new ArrayList<>();
+//      ArrayList <int> list = new ArrayList<>();
+//  ko`rinishida yozolmimiz, chunki int primitiv tip, ArrayListda esa obyektlar saqlanadi masalan String, Integer...
+    list.add("poka");
+    list.add("privet");
+    list.add("ok");
+    list.add(1, "hello");
+//     list.add(5, "ok");
+//   IndexOutOfBoundsException: Index: 5, Size: 4 ---> o`lchami 4 bo`lgan ArrayListga 5-elementni index orqali qo`shishga ruhsat bermidi
+    for (String s : list) {
+      System.out.println(s + " ");
+    }
+  }
+}
