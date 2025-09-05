f3c44cfd02c49809c067cf5718af51a8c3a94b7a
mavluda <rahimqulimavluda@gmail.com>
2025-09-05 11:59:14 +0500

    ArrayList equals

--- src/main/java/org/example/Lesson20/Test7.java
+++ src/main/java/org/example/Lesson20/Test7.java
@@ -19,6 +19,22 @@ public class Test7 {
     Collections.sort(list1);
     System.out.println(list1);
 
-  }
+    ArrayList<String> list2 = list1;
+    System.out.println(list1.equals(list2));
 
+    ArrayList<String> list3 = new ArrayList<>();
+    list3.add(s3);
+    list3.add(s1);
+    list3.add(s4);
+    list3.add(s2);
+    System.out.println(list1.equals(list3));
+    ArrayList<String> list4 = new ArrayList<>();
+    list4.add(s2);
+    list4.add(s2);
+    list4.add(s4);
+    list4.add(s3);
+    list4.add(s1);
+    System.out.println(list1.equals(list4));
+
+  }
 }
