cc95acdc046094d64e7ab8c8d0217bc77f15b8ff
mavluda <rahimqulimavluda@gmail.com>
2025-09-05 09:52:52 +0500

    ArrayList

--- src/main/java/org/example/Lesson20/Test2.java
+++ src/main/java/org/example/Lesson20/Test2.java
@@ -6,24 +6,29 @@ public class Test2 {
 
   public static void main(String[] args) {
 
-    ArrayList<String> list = new ArrayList<>();
+    ArrayList<StringBuilder> list = new ArrayList<>();
 //      ArrayList <int> list = new ArrayList<>();
 //  ko`rinishida yozolmimiz, chunki int primitiv tip, ArrayListda esa obyektlar saqlanadi masalan String, Integer...
-    list.add("poka");
-    list.add("privet");
-    list.add("ok");
-    list.add(1, "hello");
+    list.add(new StringBuilder("poka"));
+    list.add(new StringBuilder("privet"));
+    list.add(new StringBuilder("ok"));
+    list.add(1, new StringBuilder("hello"));
 //     list.add(5, "ok");
 //   IndexOutOfBoundsException: Index: 5, Size: 4 ---> o`lchami 4 bo`lgan ArrayListga 5-elementni index orqali qo`shishga ruhsat bermidi
-    for (String s : list) {
+    for (StringBuilder s : list) {
       System.out.print(s + " ");
     }
     System.out.println();
-    ArrayList<String> list2 = new ArrayList<>();
-    list2.add("Petrov");
-    list2.add("Sidorov");
+    ArrayList<StringBuilder> list2 = new ArrayList<>();
+    list2.add(new StringBuilder("Petrov"));
+    list2.add(new StringBuilder("Sidorov"));
     list.addAll(list2);
-    for(String s : list){
+    for (StringBuilder s : list) {
+      System.out.print(s + " ");
+    }
+    System.out.println();
+    list2.set(1, new StringBuilder("Ivanov"));
+    for (StringBuilder s : list) {
       System.out.print(s + " ");
     }
   }
