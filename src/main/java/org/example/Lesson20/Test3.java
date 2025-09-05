e73fac6a6585a41631e8670f0a79812eaf566430
mavluda <rahimqulimavluda@gmail.com>
2025-09-05 09:23:30 +0500

    ArrayList

---
+++ src/main/java/org/example/Lesson20/Test3.java
@@ -0,0 +1,23 @@
+package org.example.Lesson20;
+
+import java.util.ArrayList;
+
+public class Test3 {
+
+  public static void main(String[] args) {
+    ArrayList<StringBuilder> list = new ArrayList<StringBuilder>();
+    StringBuilder sb1 = new StringBuilder("hello");
+    StringBuilder sb2 = new StringBuilder("ok");
+    StringBuilder sb3 = new StringBuilder("privet");
+    list.add(sb1);
+    list.add(sb2);
+    list.add(sb3);
+    for(int i = 0; i < list.size(); i++){
+      list.get(i).append(" !!!");
+    }
+    for(int i = 0; i < list.size(); i++){
+      System.out.println(list.get(i) + " ");
+    }
+  }
+
+}
