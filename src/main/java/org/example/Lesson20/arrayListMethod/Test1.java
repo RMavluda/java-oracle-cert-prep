b2457427eca18ac8d118c164f55d7817b2255c16
mavluda <rahimqulimavluda@gmail.com>
2025-09-05 16:47:32 +0500

    ArrayList containsAll

--- src/main/java/org/example/Lesson20/arrayListMethod/Test1.java
+++ src/main/java/org/example/Lesson20/arrayListMethod/Test1.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class Test1 {
 
   public static void main(String[] args) {
-    ArrayList<String> al1 = new ArrayList <>();
+    ArrayList<String> al1 = new ArrayList<>();
     al1.add("one");
     al1.add("two");
     al1.add("three");
     al1.add("four");
     al1.add("five");
     System.out.println("al1 = " + al1);
-    ArrayList<String> al2 = new ArrayList <>();
+    ArrayList<String> al2 = new ArrayList<>();
     al2.add("one");
     al2.add("three");
     al2.add("four");
@@ -22,7 +22,14 @@ public class Test1 {
 //    al1.removeAll(al2);
 //    System.out.println("al1 = " + al1);
 
-    al1.retainAll(al2);
-    System.out.println("al1 = " + al1);
+//    al1.retainAll(al2);
+//    System.out.println("al1 = " + al1);
+
+    boolean result = al1.containsAll(al2);
+    /*
+     * contains -->> al1 da al2 ning barcha elementlari bormi?
+     * */
+    System.out.println("result = " + result);
+
   }
 }
