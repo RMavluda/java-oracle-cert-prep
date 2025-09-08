e6c4e1b39ebce189fba6fda5b64e7fa13dac94a8
mavluda <rahimqulimavluda@gmail.com>
2025-09-08 09:58:06 +0500

    ArrayList

--- src/main/java/org/example/Lesson20/arrayListMethod/Test1.java
+++ src/main/java/org/example/Lesson20/arrayListMethod/Test1.java
@@ -1,6 +1,9 @@
 package org.example.Lesson20.arrayListMethod;
 
+import static java.util.List.copyOf;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Test1 {
 
@@ -12,12 +15,13 @@ public class Test1 {
     al1.add("four");
     al1.add("five");
     System.out.println("al1 = " + al1);
-    ArrayList<String> al2 = new ArrayList<>();
-    al2.add("one");
-    al2.add("three");
-    al2.add("four");
-    al2.add("nine");
-    al2.add("ten");
+
+//    ArrayList<String> al2 = new ArrayList<>();
+//    al2.add("one");
+//    al2.add("three");
+//    al2.add("four");
+//    al2.add("nine");
+//    al2.add("ten");
 
 /*    al1.removeAll(al2);
     System.out.println("al1 = " + al1);*/
@@ -31,5 +35,33 @@ public class Test1 {
      * *//*
     System.out.println("result = " + result);*/
 
+//    List<String> sublist = al1.subList(1, 4); // == al1[1,4]
+//    System.out.println("sublist = " + sublist);
+//    sublist.add("ten");
+    /*
+    * al1 ga ham "ten" satri qo`shiladi
+    * chunki sublist al1 dan farqli bulolmidi
+    * sublist al1 ning ko`rinishi holos*/
+
+//    System.out.println("al1 = " + al1);
+//    System.out.println("sublist = " + sublist);
+//    al1.add("million");
+//    System.out.println("al1 = " + al1);
+    /*System.out.println("sublist = " + sublist);   --->>>       error bo`ladi
+    * chunki nimadir o`zgartirmoqchi bo`lsak faqat view ya`ni sublist orqali o`zgartirishimiz kerak
+    * */
+//    Object [] array = al1.toArray();
+//    String[] array2 = al1.toArray(new String [15]);
+//    for(String s : array2){
+//      System.out.println(s);
+//    }
+    List<String> list = List.of("odin", "dva", "tri");
+    System.out.println("list = " + list);
+  /* list.add("dsmfd");   exception
+  * ushbu listga boshqa qo`shimcha elementlar qo`shib bo`lmidi
+  * */
+    List<String> list2 = copyOf(al1);
+    System.out.println("list2 = " + list2);
+// ushbu list2 ni ham o`zgartirib bo`lmidi
   }
 }
