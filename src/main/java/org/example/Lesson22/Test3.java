a7abcf04260b856557caac1254c890fd639ee8fc
mavluda <rahimqulimavluda@gmail.com>
2025-09-09 12:08:49 +0500

    Encapsulation, Inheritance, Polymorphism, Super Protected

---
+++ src/main/java/org/example/Lesson22/Test3.java
@@ -0,0 +1,59 @@
+package org.example.Lesson22;
+
+public class Test3 {
+
+  public static void main(String[] args) {
+    Doctor doc = new Doctor();
+    doc.name = "Ivan";
+    doc.age = 50;
+    doc.experience = 25;
+    doc.eat();
+    doc.spetsializatsiya = "xirurg";
+    doc.sleep();
+    doc.lechit();
+
+  }
+}
+
+class Employee {
+
+  String name;
+  int age;
+  int experience;
+
+  void eat() {
+    System.out.println("Kushat");
+  }
+
+  void sleep() {
+    System.out.println("Spat");
+  }
+}
+
+class Doctor extends Employee {
+
+  String spetsializatsiya;
+
+  void lechit() {
+    System.out.println("lechit");
+  }
+}
+
+class Teacher extends Employee {
+
+  int kolvoUchenikov;
+
+  void uchit() {
+    System.out.println("uchit");
+  }
+
+}
+
+class Driver extends Employee {
+
+  String naznachenieMashin;
+
+  void vodit() {
+    System.out.println("vodit");
+  }
+}
