f73ce2d794457feccfbf47aac9797442d1a1c528
mavluda <rahimqulimavluda@gmail.com>
2025-09-10 09:12:10 +0500

    Encapsulation, Inheritance, Polymorphism, Super Protected

---
+++ src/main/java/org/example/Lesson22/Human3.java
@@ -0,0 +1,23 @@
+package org.example.Lesson22;
+
+public class Human3 {
+
+  Human3() {
+  }
+
+  String name;
+  String surname;
+
+}
+
+class Student3 extends Human3 {
+
+  Student3(String s){
+    System.out.println("Hello");
+  }
+
+
+  public static void main(String[] args) {
+    Student3 s = new Student3("ok");
+  }
+}
