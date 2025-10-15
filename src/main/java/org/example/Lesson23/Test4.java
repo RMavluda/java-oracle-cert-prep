e95642630b385192b8f7f71884d622ee1dbc38b1
mavluda <rahimqulimavluda@gmail.com>
2025-10-15 16:50:55 +0500

    Lesson 23

---
+++ src/main/java/org/example/Lesson23/Test4.java
@@ -0,0 +1,29 @@
+package org.example.Lesson23;
+
+public class Test4 {
+  // nomlanishi bir xil argumentlari xar xil bo`lgani uchun quyidagilar overloaded method hisobalnadi
+void abc(Animal a){
+  System.out.println("A");
+}
+void abc(Mouse m){
+  System.out.println("M");
+}
+
+  public static void main(String[] args) {
+    Test4 t = new Test4();
+    Animal an = new Mouse();
+    t.abc(an);
+  }
+}
+
+class Animal{
+  void getName(){
+    System.out.println("Animal");
+  }
+}
+
+class Mouse extends Animal{
+  void getName(){
+    System.out.println("Mouse");
+  }
+}
