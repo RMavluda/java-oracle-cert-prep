d29b83de3498205e6fc688f50eeaa4ca7b567780
mavluda <rahimqulimavluda@gmail.com>
2025-10-20 18:42:35 +0500

    Lesson 23 static

---
+++ src/main/java/org/example/Lesson23/A10.java
@@ -0,0 +1,43 @@
+package org.example.Lesson23;
+
+public class A10 {
+
+  String s1 = "privet";
+  static double pi = 3.14;
+
+  int summa(int... i) {
+    int result = 0;
+    for (int a : i) {
+      result += a;
+    }
+    return result;
+  }
+
+  static void abc() {
+    System.out.println("Static method");
+  }
+
+}
+
+class B10 extends A10 {
+
+//  String s1 = super.s1 + ",s1 drug!";
+
+  double pi = super.pi;
+  String s1 = super.s1;
+//  boolean s1 = true;
+
+  int summa(int... i) {
+    int result = super.summa(i);
+    System.out.println("Summa: " + result);
+    super.abc();
+    return result;
+  }
+
+
+  public static void main(String[] args) {
+    B10 b = new B10();
+    System.out.println(b.s1);
+//    System.out.println(super.s1);
+  }
+}
