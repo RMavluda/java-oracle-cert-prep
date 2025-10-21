95fad8a4c5708f21626ecd8892bdc80810dc5220
mavluda <rahimqulimavluda@gmail.com>
2025-10-21 18:42:23 +0500

    Lesson 23 Homework Test3

---
+++ src/main/java/org/example/Homework/Lesson_23/X3.java
@@ -0,0 +1,32 @@
+package org.example.Homework.Lesson_23;
+
+public class X3 {
+
+  public X3() {
+    System.out.println('X');
+  }
+
+  public X3(int i) {
+    System.out.println('X' + i);
+  }
+
+  private boolean abc() {
+    return false;
+  }
+
+  public static void main(String[] args) {
+    X3 x = new Y3(18);
+    System.out.println(x.abc());
+  }
+}
+
+class Y3 extends X3 {
+
+  public Y3(int i) {
+    System.out.println('Y');
+  }
+
+  public boolean abc() {
+    return true;
+  }
+}
