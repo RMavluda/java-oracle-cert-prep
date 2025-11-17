17a8b532d4f3b7908cabf9a4a34011563d770336
mavluda <rahimqulimavluda@gmail.com>
2025-11-17 17:18:40 +0500

    Lesson 24 interface - default realization and access modifier

--- src/main/java/org/example/Lesson24/Test5.java
+++ src/main/java/org/example/Lesson24/Test5.java
@@ -10,9 +10,10 @@ interface I1 {
   // interface dagi method oldida yozilgan default bu access modifier ni anglatmidi
   // ushbu default shuni anglatadiki -> ushbu methodda default realizatsiya mavjud deganidir
 
-  default void ghi(){
+  default void ghi() {
     System.out.println("eto method ghi");
   }
+
   default void def() {
     System.out.println("def");
   }
@@ -21,16 +22,18 @@ interface I1 {
 class Z2 implements I1 {
 
   public void abc() {
-    System.out.println("eto metod abc");}
-    public static void main(String [] args){
-      Z2 z = new Z2();
-      z.abc();
-      z.def();
-      z.ghi();
-    }
-// interface dagi barcha methodlar access modifieri public hisoblanadi
-    public void ght() {
-      System.out.println("Eto metod ghi");
+    System.out.println("eto metod abc");
   }
-    }
+
+  public static void main(String[] args) {
+    Z2 z = new Z2();
+    z.abc();
+    z.def();
+    z.ghi();
+  }
+
+  // interface dagi barcha methodlar access modifieri public hisoblanadi
+  public void ght() {
+    System.out.println("Eto metod ghi");
+  }
 }
