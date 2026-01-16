cd2a3597f0a7e53a339bfe0bdd961f05d0ded5d2
mavluda <rahimqulimavluda@gmail.com>
2026-01-16 17:47:21 +0500

    Lesson 25

---
+++ src/main/java/org/example/Lesson25/Test1.java
@@ -0,0 +1,71 @@
+package org.example.Lesson25;
+
+public class Test1 {
+
+  public static void main(String[] args) {
+
+//    Driver[] array1 = {new Driver(), new Driver()};
+//    Employee[] array2 = {new Driver(), new Driver(), new Teacher(), new Doctor()};
+//    Help_able[] array3 = {new Driver(), new Driver(), new Teacher(), new Doctor()};
+
+    Employee emp1 = new Teacher();
+    Employee emp2 = new Driver();
+    Employee emp3 = new Doctor();
+    Employee [] arr = {emp1, emp2, emp3};
+
+
+//    Help_able h = new Teacher();
+//    h.help();
+//    emp1.help();
+//
+//    emp1.work();
+//    emp2.work();
+//    emp3.work();
+  }
+}
+
+abstract class Employee implements Help_able {
+
+  void sleep() {
+    System.out.println("Employee sleeps");
+  }
+
+  abstract void work();
+
+}
+
+class Teacher extends Employee implements Help_able {
+
+  public void help() {
+    System.out.println("Teacher helps");
+  }
+
+  void work() {
+    System.out.println("Teacher works");
+  }
+}
+
+class Driver extends Employee implements Help_able {
+
+  public void help() {
+  }
+
+  void work() {
+    System.out.println("Driver works");
+  }
+}
+
+class Doctor extends Employee implements Help_able {
+
+  public void help() {
+  }
+
+  void work() {
+    System.out.println("Doctor works");
+  }
+}
+
+interface Help_able {
+
+  void help();
+}
