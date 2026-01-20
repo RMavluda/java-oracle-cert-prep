f26956b1bbb8978815c52032377970af3f8d8cfa
mavluda <rahimqulimavluda@gmail.com>
2026-01-20 17:00:35 +0500

    Lesson 25 primitive data types casting

---
+++ src/main/java/org/example/Lesson25/Test6.java
@@ -0,0 +1,50 @@
+package org.example.Lesson25;
+
+// Primitive data types casting
+public class Test6 {
+
+  public static void main(String[] args) {
+///*
+//* casting orqali katta type larni kichik typelarga yozish mumkin
+//* */
+//    byte b = 10;
+//    int a = b;
+//    System.out.println(7);
+//
+//    /*
+//     * int --> byte short char bilan casting bo`la oladi, qachonki:
+//     * int constanta bo`lganida
+//     * int qiyamti berilgan qiymat oralig`iga mos bo`lganida
+//     * */
+//    int i1 = 3;
+////    final int i1 = 3;
+//    short s2 = (short) i1;
+//    byte b2 = 127;
+//    short s1 = -6;
+//    char c1 = 100;
+//
+//    long l1 = 1;
+//    int i2 = (int) l1;
+//
+//
+//    int i10 = 111111;
+//    short s10 = (short) i10;
+//    System.out.println(s10); // bu holatda int typedagi qiymat short type ga sig`maganligi uchun. chegaraning aylanib minus qiymatga o`tib ketadi
+//
+//    char ch10 = (char)-8; // bu yerda ham castingda char manfiy qiyamt qabul qilamsdan, qandaydir musbat qiymatga o`zgartirib yuboradi
+//    System.out.println(ch10);
+//
+//    boolean b10 = true;
+////    int i11 = (int)b10;
+//    int i12 = (int)3.1415926535;
+//    System.out.println(i12);
+//
+//    double d = 59845945.2323;
+//    byte b12 = (byte)d;
+//    System.out.println(b12);
+
+    int i = 2147483647;
+    System.out.println(i + 1);
+  }
+
+}
