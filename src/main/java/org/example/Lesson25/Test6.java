56f4e4d9a04d8354d9d518614f081a4c239c6db7
mavluda <rahimqulimavluda@gmail.com>
2026-01-20 17:21:48 +0500

    Lesson 25 primitive data types casting

--- src/main/java/org/example/Lesson25/Test6.java
+++ src/main/java/org/example/Lesson25/Test6.java
@@ -43,8 +43,46 @@ public class Test6 {
 //    byte b12 = (byte)d;
 //    System.out.println(b12);
 
-    int i = 2147483647;
-    System.out.println(i + 1);
+//    int i = 2147483647;
+//    System.out.println(i + 1);
+
+    int i = 5;
+    long l = 10;
+    System.out.println(i + 1); // avvaliga i long tipiga konvertatsiya bo`ladi va + 1 orqali long tipidagi javob ko`rsatialdi
+//    i = i * l;   bu yerda: i * l, avvaliga i long typega o`tadi va long ga ko`paytiriladi. ya`ni long * long. long * long ni castingsiz int i o`zgaruvchisiga tenglolmimiz
+//    i = (int)i * l;
+    i *= l; // bu yerda: i * l, i longga konvertatsiya bo`ladi. l shundog`am long. long * long = long. keyin bu long int bn casting(JAVADA BU KABI AMALLAR UCHUN YASHIRIN CAST MAVJUD) bo`ladi.
+
+    float f = 3.14f;
+    int i2 = 10;
+    System.out.println(f + i2); //float
+
+    byte b = 3;
+    short s = 4;
+    char c = 5;
+    System.out.println(b + s + c); // int
+    System.out.println(b++); // byte
+
+
+    byte b2 = 10;
+    float f2 = 7;
+    double d2 = 2.0;
+    System.out.println(b2 + f2 - d2); // double
+
+    double d10 = 3.14;
+    float f10 = 6.28f;
+    System.out.println(d10 + f10); // double
+
+    float f11 = 0*1.5f;
+//    or
+    float f12 = 0 * (float)1.5;
   }
 
 }
+
+
+/*
+*       QOIDA 1:         *=, +=, -= kabi compound assignment operatorlar o‘z ichida yashirin cast qiladi.
+*       QOIDA 2:         agar ikkita butun va vergulli sonalr casting qilinsa, java avtomatik vergulli songa kenvertatsiya qiladi.
+*       QOIDA 3:         agar short, byte, char lar casting bolganida, hatto int qanashmayotgan bo`lsa ham int ga konvertatsiya bo`ladi. faqat unar operator ++ da bu ishlamaydi
+ * */
