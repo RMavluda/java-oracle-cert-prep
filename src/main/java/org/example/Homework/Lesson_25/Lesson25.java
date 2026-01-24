fc0175083fc858b63a62397160045aea4f3eac6c
mavluda <rahimqulimavluda@gmail.com>
2026-01-24 18:13:59 +0500

    Lesson 25 Homework: Refactor Animal hierarchy,

--- src/main/java/org/example/Homework/Lesson_25/Lesson25.java
+++ src/main/java/org/example/Homework/Lesson_25/Lesson25.java
@@ -1,11 +1,48 @@
 package org.example.Homework.Lesson_25;
 
 public class Lesson25 {
-
-
   public static void main(String[] args) {
+    Animal a1 = new Mechenosec("Karl1");
+    Animal a2 = new Penguin("Kovalski1");
+    Animal a3 = new Lion("Leva1");
 
+    Fish f1 = new Mechenosec("Karl2");
+    Bird b1 = new Penguin("Kovalski2");
+    Mammal m1 = new Lion("Leva2");
 
+    Mechenosec mech1 = new Mechenosec("Karl3");
+    Penguin p1 = new Penguin("Kovalski3");
+    Lion l1 = new Lion("Leva3");
+
+    Speakable s1 = new Penguin("Kovalski4");
+    Speakable s2 = new Lion("Leva4");
+    Animal[] array1 = {a1, a2, a3, f1, b1, m1, mech1, p1, l1};
+    Speakable[] array2 = {s1, s2, b1, m1, p1, l1};
+
+    for (Animal a : array1) {
+      if (a instanceof Mechenosec) {
+        Mechenosec m = (Mechenosec) a;
+        System.out.println(m.name);
+        m.eat();
+        m.sleep();
+        m.swim();
+      } else if (a instanceof Penguin) {
+        Penguin p = (Penguin) a;
+        System.out.println(p.name);
+        p.eat();
+        p.sleep();
+        p.fly();
+        p.speak();
+      } else if (a instanceof Lion) {
+        Lion l = (Lion) a;
+        System.out.println(l.name);
+        l.eat();
+        l.sleep();
+        l.run();
+        l.speak();
+      }
+      System.out.println("---------------------------->-->-->");
+    }
   }
 }
 
@@ -18,7 +55,6 @@ abstract class Animal {
   String name;
 
   abstract void eat();
-
   abstract void sleep();
 }
 
@@ -29,23 +65,22 @@ abstract class Fish extends Animal {
   }
 
   public void sleep() {
-    System.out.println("Vsegda interecno nablyudat, akk spyat ribi");
+    System.out.println("Vsegda interesno nablyudat, kak spyat ryby");
   }
 
   abstract void swim();
-
 }
 
 abstract class Bird extends Animal implements Speakable {
 
   Bird(String name) {
-    super(name); // bu orqali super class constructoriga qiymat yuboriladi
+    super(name); // bu orqali super class konstruktori ga qiymat yuboriladi
   }
 
   abstract void fly();
 
   public void speak() {
-    System.out.println(name + "sings");
+    System.out.println(name + " sings");
   }
 }
 
@@ -72,11 +107,11 @@ class Mechenosec extends Fish {
   }
 
   public void swim() {
-    System.out.println("Mehcenosec krasivaya riba, kotoraya bistro plavayet!");
+    System.out.println("Mechenosec — krasivaya ryba, kotoraya bystro plavayet!");
   }
 
   public void eat() {
-    System.out.println("Mehcenosec ne xishnaya riba, i ona est obishniy ribiy krom!");
+    System.out.println("Mechenosec ne xishchnaya ryba, i ona yest obychnyy rybiy korm!");
   }
 }
 
@@ -87,11 +122,11 @@ class Penguin extends Bird {
   }
 
   public void eat() {
-    System.out.println("Penguin lyubit yest ribu");
+    System.out.println("Penguin lyubit yest rybu");
   }
 
   public void sleep() {
-    System.out.println("Penguins spyat prijavshiy drug k drugu");
+    System.out.println("Penguins spyat, prizhavshis drug k drugu");
   }
 
   public void fly() {
@@ -99,18 +134,18 @@ class Penguin extends Bird {
   }
 
   public void speak() {
-    System.out.println("Penguins ne umeyut pet tak solovyi");
+    System.out.println("Penguins ne umeyut pet, kak solovyi");
   }
 }
 
-class Lion extends  Mammal {
+class Lion extends Mammal {
 
   Lion(String name) {
     super(name);
   }
 
   public void eat() {
-    System.out.println("Lev kak lyuvboy xishnik, lyubit myaso");
+    System.out.println("Lev, kak lyuboy xishchnik, lyubit myaso");
   }
 
   public void sleep() {
@@ -118,9 +153,6 @@ class Lion extends  Mammal {
   }
 
   public void run() {
-    System.out.println("Lev-eto ne samaya bistraya koshka!");
+    System.out.println("Lev — eto ne samaya bystraya koshka!");
   }
-
 }
-
-
