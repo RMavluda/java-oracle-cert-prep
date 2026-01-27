6d97c1a576900f92c71192b4d43edf36f136e767
mavluda <rahimqulimavluda@gmail.com>
2026-01-27 16:59:36 +0500

    Lesson 26 Update Test5 to include overloaded constructors and static initialization blocks

--- src/main/java/org/example/Lesson26/Test5.java
+++ src/main/java/org/example/Lesson26/Test5.java
@@ -2,22 +2,27 @@ package org.example.Lesson26;
 
 public class Test5 {
     // quyida Initialized block
-    Test5(){
-        System.out.println("Eto konstruktor");
-    }
-
     {
         System.out.println("Eto init block3");
     }
+
+    Test5(){
+        System.out.println("Eto konstruktor 1");
+    }
+
+    Test5(int a){
+        this();
+        System.out.println("Eto konstruktor2");
+    }
+
 
     {
         System.out.println("Eto init block1");
     }
 
-    {
-        System.out.println("Eto init block2");
+    static{
+        System.out.println("STATIC init block 1");
     }
-
 
     String s1;
     {
@@ -30,13 +35,22 @@ public class Test5 {
 //        ekrancha chop etish uchun avval ushbu o`zgaruvchini e`lon qilgan bo`lishimiz kerak
     }
     String s2;
+
+    {
+        System.out.println("Eto init block2");
+    }
+
+    static{
+        System.out.println("STATIC init block 2");
+    }
+
 /*
 *
 * Initialized block qachonki ushbu klassdan obyekt yaratilganida har safar ishga tushadi
 *
 * */
     public static void main(String[] args) {
-        Test5 t = new Test5();
         Test5 t1 = new Test5();
+        Test5 t2 = new Test5(5);
     }
 }
