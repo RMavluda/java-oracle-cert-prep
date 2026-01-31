504e42e9e0c13e3109b4fa043542d120d428892a
mavluda <rahimqulimavluda@gmail.com>
2026-01-31 23:10:32 +0500

    Lesson 27 Test4 check-exceptions: add try-catch block for FileNotFoundException/IOException
    Signed-off-by: mavluda <rahimqulimavluda@gmail.com>

--- src/main/java/org/example/Lesson27/Test4.java
+++ src/main/java/org/example/Lesson27/Test4.java
@@ -3,21 +3,64 @@ package org.example.Lesson27;
 import java.io.*;
 
 public class Test4 {
-    public static void main(String[] args) {
-        File f = new File("test4.txt");
+
+    void abc() throws FileNotFoundException {
+        File f = new File("test8.txt");
+//        try {
+        FileInputStream fis = new FileInputStream(f);
+//            fis.read();
+        System.out.println("Have a good day");
+//        } catch (FileNotFoundException e) { // tutib olish kerak bo`lgan exception turi
+//            System.out.println("abc ==> Exception tutib olindi -----> " + e);
+//        }
+    }
+
+    void ghi() {
+        String s = null;
+        System.out.println(s.length());
+    }
+
+    void def() throws FileNotFoundException {
+        System.out.println("Hello");
         try {
-            FileInputStream fis = new FileInputStream(f);
-            System.out.println("Have a good day");
-        } catch (FileNotFoundException e) { // tutib olish kerak bo`lgan exception turi
-            System.out.println("Exception tutib olindi -----> " + e);
-        } catch (
-                NullPointerException e) { // aslida RunTimeExceptionlarni catch orqali yozish da mano yo`q, yozish tavsiya qilinmidi
-            System.out.println("Exception tutib olindi -----> " + e);
-        } finally {
-            System.out.println("bu finally block");
+            abc();
+        } catch (FileNotFoundException e) {
+            System.out.println("def ==> Exception tutib olindi -----> " + e);
         }
 
-        System.out.println("Ushbu code ortiq exception bilan aloqador emas");
+        /*
+         * try catch bilan ishlansa throws orqali exceptionni ko`rsatish shart emas
+         * */
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Test4 t = new Test4();
+//        try {
+        t.def();
+//        }catch(FileNotFoundException e){
+//            System.out.println("main ==> Exception tutib olindi -----> " + e);
+//        }
+        t.ghi();
+//        File f = new File("test4.txt");
+//        try {
+//            FileInputStream fis = new FileInputStream(f);
+//            fis.read();
+//            System.out.println("Have a good day");
+//        } catch (FileNotFoundException e) { // tutib olish kerak bo`lgan exception turi
+//            System.out.println("Exception tutib olindi -----> " + e);
+//        }
+//        catch(IOException e){
+//            System.out.println("Exception tutib olindi -----> " + e);
+//        }
+//        catch (
+//                NullPointerException e) { // aslida RunTimeExceptionlarni catch orqali yozish da mano yo`q, yozish tavsiya qilinmidi
+//            System.out.println("Exception tutib olindi -----> " + e);
+//        } finally {
+//            System.out.println("bu finally block");
+//        }
+
+//        System.out.println("Ushbu code ortiq exception bilan aloqador emas");
     }
 }
 
