a4e51246b8109b7a1969b73b8170c325321488fa
mavluda <rahimqulimavluda@gmail.com>
2026-01-31 23:13:14 +0500

    Lesson 27 Test4 exceptions: LIFO - last in first out
    Signed-off-by: mavluda <rahimqulimavluda@gmail.com>

--- src/main/java/org/example/Lesson27/Test4.java
+++ src/main/java/org/example/Lesson27/Test4.java
@@ -3,16 +3,11 @@ package org.example.Lesson27;
 import java.io.*;
 
 public class Test4 {
-
+    // LIFO - last in first out
     void abc() throws FileNotFoundException {
         File f = new File("test8.txt");
-//        try {
         FileInputStream fis = new FileInputStream(f);
-//            fis.read();
         System.out.println("Have a good day");
-//        } catch (FileNotFoundException e) { // tutib olish kerak bo`lgan exception turi
-//            System.out.println("abc ==> Exception tutib olindi -----> " + e);
-//        }
     }
 
     void ghi() {
@@ -22,45 +17,13 @@ public class Test4 {
 
     void def() throws FileNotFoundException {
         System.out.println("Hello");
-        try {
             abc();
-        } catch (FileNotFoundException e) {
-            System.out.println("def ==> Exception tutib olindi -----> " + e);
-        }
-
-        /*
-         * try catch bilan ishlansa throws orqali exceptionni ko`rsatish shart emas
-         * */
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
         Test4 t = new Test4();
-//        try {
         t.def();
-//        }catch(FileNotFoundException e){
-//            System.out.println("main ==> Exception tutib olindi -----> " + e);
-//        }
-        t.ghi();
-//        File f = new File("test4.txt");
-//        try {
-//            FileInputStream fis = new FileInputStream(f);
-//            fis.read();
-//            System.out.println("Have a good day");
-//        } catch (FileNotFoundException e) { // tutib olish kerak bo`lgan exception turi
-//            System.out.println("Exception tutib olindi -----> " + e);
-//        }
-//        catch(IOException e){
-//            System.out.println("Exception tutib olindi -----> " + e);
-//        }
-//        catch (
-//                NullPointerException e) { // aslida RunTimeExceptionlarni catch orqali yozish da mano yo`q, yozish tavsiya qilinmidi
-//            System.out.println("Exception tutib olindi -----> " + e);
-//        } finally {
-//            System.out.println("bu finally block");
-//        }
-
-//        System.out.println("Ushbu code ortiq exception bilan aloqador emas");
     }
 }
 
