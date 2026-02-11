caa4d97765607075ff5c7b6729c5641709d5e387
mavluda <rahimqulimavluda@gmail.com>
2026-02-11 18:13:03 +0500

    Lesson 28 Example ExceptionInInitializerError and ArrayIndexOutOfBoundsException and NPE

---
+++ src/main/java/org/example/Lesson28/Test10.java
@@ -0,0 +1,23 @@
+package org.example.Lesson28;
+
+public class Test10 {
+
+//  static{
+//    int a = Integer.parseInt("12e");
+//  }
+
+//  static String s = null;
+//  static int a = s.length();
+
+  static String s = abc();
+
+  static String abc() {
+    String[] array = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    return array[100];
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
