7bde5ed826b672345819cf57b9b76e255da04aa8
mavluda <rahimqulimavluda@gmail.com>
2026-02-11 18:35:06 +0500

    Errors

--- src/main/java/org/example/Lesson28/Test12.java
+++ src/main/java/org/example/Lesson28/Test12.java
@@ -1,29 +1,44 @@
 package org.example.Lesson28;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Test12 {
 
   public static void main(String[] args) {
-    Animal animal = new Mouse();
-    try {
-      animal.run();
-    }catch(IOException e){
-      System.out.println("Exception is catch: " + e);
-    }
+//    Animal animal = new Mouse();
 
-    }
+
+  }
 
 }
 
-class Animal{
-  void run()throws IOException{
-    System.out.println("Animal runs");
+class Animal {
+
+  Animal() throws FileNotFoundException, IndexOutOfBoundsException {
   }
 }
 
-class Mouse extends Animal{
-  void run() {
-    System.out.println("Mouse runs");
+class Mouse extends Animal {
+
+  Mouse() throws Exception, IOException, ArrayIndexOutOfBoundsException {
+    super();
   }
+}
+
+class Human {
+
+  String name;
+  int age;
+
+  Human(String name, int age) throws Exception, FileNotFoundException, IndexOutOfBoundsException {
+
+    if (age < 0) {
+      throw new Exception("nekorrektniy vozrast");
+    }
+    this.name = name;
+    this.age = age;
+  }
+
+
 }
