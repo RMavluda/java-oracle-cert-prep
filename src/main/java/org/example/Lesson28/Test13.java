58c7f83ba780e82f2a13a13c2e8be1cba6558a24
mavluda <rahimqulimavluda@gmail.com>
2026-02-11 18:38:41 +0500

    unreachable exceptions

---
+++ src/main/java/org/example/Lesson28/Test13.java
@@ -0,0 +1,18 @@
+package org.example.Lesson28;
+
+import java.io.*;
+
+public class Test13 {
+
+  static void abc() {
+    System.out.println("ok");
+  }
+
+  public static void main(String[] args) {
+//    try {
+//      abc();
+//    } catch (IOException e) {
+//
+//    }
+  }
+}
