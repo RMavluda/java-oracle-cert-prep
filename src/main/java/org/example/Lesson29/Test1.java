4139f5f242c213b076a516e4705539398b7c58a7
mavluda <rahimqulimavluda@gmail.com>
2026-02-12 10:05:16 +0500

    Local date/time/datetime

---
+++ src/main/java/org/example/Lesson29/Test1.java
@@ -0,0 +1,18 @@
+package org.example.Lesson29;
+
+import java.time.*;
+
+public class Test1 {
+  public static void main(String[] args) {
+    System.out.println(LocalDate.now());
+    System.out.println(LocalTime.now());
+    System.out.println(LocalDateTime.now());
+
+
+    /*LocalDate localDate = new LocalDate(2015,05,13);
+    *bu korinishda yaratish mumkin emas, chunki LocalDate private constructorga ega
+    * */
+
+  }
+
+}
